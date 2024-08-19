eabbe197be902e194359c3fab41673add2374c2d
IzzatMuzhaffar <izzatmuz@gmail.com>
2024-08-19 23:09:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# emoji-quiz-app
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/IzzatMuzhaffar/emoji-quiz-app)
---
+++ script.js
@@ -0,0 +1,41 @@
+function startQuiz() {
+  const guess = prompt('What does this ✅ stands for?');
+
+  if (guess.toLowerCase() === 'tick') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Tick.');
+  }
+
+  const guess2 = prompt('What does this 🍉 stands for?');
+
+  if (guess2.toLowerCase() === 'watermelon') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Watermelon.');
+  }
+
+  const guess3 = prompt('What does this ❤️ stands for?');
+
+  if (guess3.toLowerCase() === 'heart') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Heart.');
+  }
+
+  const guess4 = prompt('What does this 🍔 stands for?');
+
+  if (guess4.toLowerCase() === 'burger') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Burger.');
+  }
+
+  const guess5 = prompt('What does this 😭 stands for?');
+
+  if (guess5.toLowerCase() === 'crying') {
+    alert('You guessed correctly!');
+  } else {
+    alert('Sorry, the correct answer was Crying.');
+  }
+}
